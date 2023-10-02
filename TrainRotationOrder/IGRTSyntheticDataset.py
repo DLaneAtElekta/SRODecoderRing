90ae424ea75ff272d4a4aac6462e67c47c852b5c
Derek Lane <dglane001@gmail.com>
2023-10-01 22:11:57 -0500

    IGRT synthetic data set

---
+++ TrainRotationOrder/IGRTSyntheticDataset.py
@@ -0,0 +1,70 @@
+from random import random, choices
+import numpy as np
+from scipy.spatial.transform import Rotation
+
+
+def patient_position_to_quarter_rotation(patient_position):
+    match patient_position.lower():
+        case "hfs":
+            return np.array([0, 0, 0], dtype=float)
+        case "hfp":
+            return np.array([2, 0, 0], dtype=float)
+        case _:
+            raise ("bad string")
+
+
+def patient_to_iec_room_shift(quarter_rotations, patient_shift):
+    rot = Rotation.from_euler("xyz", 90 * quarter_rotations, degrees=True)
+    return np.append(patient_shift[:3], rot.apply(patient_shift[3:]))
+
+
+def iec_room_to_mac_shift(mac_dirs, iec_room_shift):
+    return mac_dirs * iec_room_shift
+
+
+def generate_patient_chart(num_sessions=3):
+    tensor = []
+    labels = []
+
+    rng = np.random.default_rng()
+    # rng.choice()
+
+    patient_position = choices(["hfs", "hfp"], weights=[90, 10])[0]
+    quarter_rotations = patient_position_to_quarter_rotation(patient_position)
+    tensor += [quarter_rotations]
+    labels += ["quater_rotations"]
+
+    mac_dirs = np.array([1, 1, 1, 1, 1, 1])
+    tensor += [mac_dirs[:3]]
+    labels += ["mac xlate dir"]
+    tensor += [mac_dirs[3:]]
+    labels += ["mac rot dir"]
+
+    init_couch = np.array([0, 0, 0, 0, 0, 0])
+    tensor += [init_couch[:3]]
+    labels += ["init_couch xlate"]
+    tensor += [init_couch[3:]]
+    labels += ["init_couch rot"]
+
+    for n in range(num_sessions):
+        patient_shift = rng.standard_normal(6)
+
+        rotation_matrix = Rotation.from_euler("xyz", patient_shift[3:], degrees=True)
+        rotation_matrix = rotation_matrix.as_matrix()
+        tensor += [rotation_matrix[0]]
+        tensor += [rotation_matrix[1]]
+        tensor += [rotation_matrix[2]]
+
+        iec_room_shift = patient_to_iec_room_shift(quarter_rotations, patient_shift)
+        mac_shift = iec_room_to_mac_shift(
+            mac_dirs=mac_dirs, iec_room_shift=iec_room_shift
+        )
+        tensor += [mac_shift[:3]]
+        tensor += [mac_shift[3:]]
+
+    return tensor
+
+
+from rich import print
+
+print(generate_patient_chart(num_sessions=3))
